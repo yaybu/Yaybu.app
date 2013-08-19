f064b3c6505288ab7a7a69704dffd832f3a4acb9
John Carr <john.carr@isotoma.com>
2013-08-19 23:02:22 +0100

    Make sure lextab/parsetab is shipped in Yaybu.app

--- YaybuShell.py
+++ YaybuShell.py
@@ -1,4 +1,8 @@
 if __name__ == "__main__":
     from yaybu.core.main import main
+    # These imports are to force modules into site-packages.zip
+    # (yay py2app)
+    from yay import lextab
+    from yay import parsetab
     main()
 
