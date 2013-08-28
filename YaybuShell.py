095fccb0882572e740ed1ddd858ea62747991004
John Carr <john.carr@isotoma.com>
2013-08-28 18:29:15 +0100

    Use the bundled certs

--- YaybuShell.py
+++ YaybuShell.py
@@ -1,8 +1,20 @@
+from yaybu.core.main import main
+
+# These imports are to force modules into site-packages.zip
+# (yay py2app)
+from yay import lextab
+from yay import parsetab
+
+# CA certs on darwin are in the system keyring - they can be readily accessed with commands like:
+#   security export -k /System/Library/Keychains/SystemCACertificates.keychain -t certs
+# However i'm not sure how libcloud/python can take a stream of certs - it looks like the certs have to exist on disk!!
+# So tell libcloud where our bundled certs are
+import os
+import libcloud.security
+libcloud.security.CA_CERTS_PATH.append(
+    os.path.join(os.environ["RESOURCEPATH"], "cacert.pem")
+        )
+
 if __name__ == "__main__":
-    from yaybu.core.main import main
-    # These imports are to force modules into site-packages.zip
-    # (yay py2app)
-    from yay import lextab
-    from yay import parsetab
     main()
 
